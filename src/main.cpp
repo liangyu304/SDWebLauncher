2467c162100e03de78ca361859c5f91f214ca030
liangyu304 <39788761+liangyu304@users.noreply.github.com>
2023-05-27 23:48:44 +0800

    add new 3trd fluentui (#5)
    Co-authored-by: liangyuRan <liangyu.ran@xyzrobotics.com>

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,53 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 #include <QQmlContext>
-#include "test/backend.h"
+#include <QDir>
+#include <QQuickWindow>
+#include <QProcess>
+#include <FramelessHelper/Quick/framelessquickmodule.h>
+#include <FramelessHelper/Core/private/framelessconfig_p.h>
+#include "AppInfo.h"
+#include <QFile>
+#include <QDebug>
+
+FRAMELESSHELPER_USE_NAMESPACE
 
 int main(int argc, char *argv[])
 {
+    //将样式设置为Basic，不然会导致组件显示异常
+    qputenv("QT_QUICK_CONTROLS_STYLE","Basic");
+    FramelessHelper::Quick::initialize();
     QGuiApplication app(argc, argv);
-
-    Backend backend;
-
+#ifdef Q_OS_WIN // 此设置仅在Windows下生效
+    FramelessConfig::instance()->set(Global::Option::ForceHideWindowFrameBorder);
+#endif
+    FramelessConfig::instance()->set(Global::Option::DisableLazyInitializationForMicaMaterial);
+    FramelessConfig::instance()->set(Global::Option::CenterWindowBeforeShow);
+    FramelessConfig::instance()->set(Global::Option::ForceNonNativeBackgroundBlur);
+    FramelessConfig::instance()->set(Global::Option::EnableBlurBehindWindow);
+#ifdef Q_OS_MACOS
+    FramelessConfig::instance()->set(Global::Option::ForceNonNativeBackgroundBlur,false);
+#endif
+    AppInfo* appInfo = new AppInfo();
+    IPC ipc(0);
+    if(!appInfo->isOwnerProcess(&ipc)){
+        return 0;
+    }
+    app.setQuitOnLastWindowClosed(false);
     QQmlApplicationEngine engine;
-    engine.addImportPath(TaoQuickImportPath);
+    FramelessHelper::Quick::registerTypes(&engine);
+    appInfo->init(&engine);
 
-    engine.rootContext()->setContextProperty("taoQuickImportPath", TaoQuickImportPath);
+//    const QUrl url(u"qrc:/SdWebLauncher/src/qml/main.qml"_qs);
+    const QUrl url("qrc:/main.qml");
 
-    engine.rootContext()->setContextProperty("backend", &backend);
-
-    const QUrl url(u"qrc:/SdWebLauncher/src/qml/main.qml"_qs);
+//    const QUrl url(u"qrc:/SdWebLauncher/src/qml/window/.qml"_qs);
+    //  const QUrl url(QStringLiteral("qrc:/example/qml/TestWindow.qml"));
     QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
-                     &app, [url](QObject *obj, const QUrl &objUrl) {
-        if (!obj && url == objUrl)
-            QCoreApplication::exit(-1);
-    }, Qt::QueuedConnection);
+        &app, [url](QObject *obj, const QUrl &objUrl) {
+            if (!obj && url == objUrl)
+                QCoreApplication::exit(-1);
+        }, Qt::QueuedConnection);
     engine.load(url);
-
     return app.exec();
 }
